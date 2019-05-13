7e2975ae9d86baa35705f168ad61f74f24616f03
Hidde Hulshof <hidhulshof@gmail.com>
2019-05-13 14:00:29 +0200

    Footer en header

--- resources/views/layouts/partials/footer.blade.php
+++ resources/views/layouts/partials/footer.blade.php
@@ -1,8 +1,47 @@
 <footer class="text-muted bg-dark h-25 mt-5">
     <div class="container">
-        <p class="float-right">
-            <a href="#">Back to top</a>
-        </p>
-        <p>Hier komt de Footer</p>
+        <div class="row">
+            <div class="col-md-4">
+                <ul>
+                    <li>
+                        <p>test</p>
+                    </li>
+                    <li>
+                        <p>test</p>
+                    </li>
+                    <li>
+                        <p>test</p>
+                    </li>
+                </ul>
+            </div>
+            <div class="col-md-4">
+                <ul>
+                    <li>
+                        <p>test</p>
+                    </li>
+                    <li>
+                        <p>test</p>
+                    </li>
+                    <li>
+                        <p>test</p>
+                    </li>
+
+                </ul>
+            </div>
+            <div class="col-md-4">
+                <ul>
+                    <li>
+                        <p>test</p>
+                    </li>
+                    <li>
+                        <p>test</p>
+                    </li>
+                    <li>
+                        <p>test</p>
+                    </li>
+                </ul>
+            </div>
+        </div>
+
     </div>
 </footer>
