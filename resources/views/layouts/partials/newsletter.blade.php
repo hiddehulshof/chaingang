917f78f27f7c16200aa198bfd350e71dca28865f
Guusboyy <38752370+Guusboyy@users.noreply.github.com>
2019-05-17 10:06:23 +0200

    Article files

--- resources/views/layouts/partials/newsletter.blade.php
+++ resources/views/layouts/partials/newsletter.blade.php
@@ -1,8 +1,12 @@
 <div class="newsletter">
     <div class="container">
         <div class="row justify-content-center">
-            <div class="col-md-6 ">
+            <div class="col-md-6">
                 <h2 class="newsletter__heading">Schrijf je in voor onze nieuwsbrief</h2>
+                <form>
+                    <input class="textbox" type="email" name="email"><br>
+                    <input class="button" type="button" name="submit" value="Inschrijven">
+                </form>
             </div>
         </div>
     </div>
