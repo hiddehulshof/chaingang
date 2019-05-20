f17bbfe6a6b1409d9816ed4a71275f37b23ec6ef
Niels Bosman <nielsbosman33@icloud.com>
2019-05-20 13:00:30 +0200

    Fixes

--- resources/views/layouts/partials/newsletter.blade.php
+++ resources/views/layouts/partials/newsletter.blade.php
@@ -4,7 +4,7 @@
             <div class="col">
                 <h2 class="newsletter__heading">Schrijf je in voor onze nieuwsbrief</h2>
                 <form>
-                    <input class="textbox newsletter__input" type="email" name="email">
+                    <input class="textbox newsletter__input" type="email" name="email" placeholder="Email">
                     <input class="button newsletter__input" type="button" name="submit" value="Inschrijven">
                 </form>
             </div>
