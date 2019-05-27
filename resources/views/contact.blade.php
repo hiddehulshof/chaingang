caa540d79cc2830cf12504bcfdd2d45166c173d9
Guusboyy <38752370+Guusboyy@users.noreply.github.com>
2019-05-27 13:20:26 +0200

    contactform + googlemaps menu over header fix

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -5,8 +5,18 @@
 @endsection
 
 @section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4">
+                @include('layouts.partials.contactinfo')
+            </div>
+            <div class="col-md-8">
+                @include('layouts.partials.contactform')
+            </div>
 
-    @include('layouts.partials.contactform')
+        </div>
+    </div>
+    @include('layouts.partials.maps')
     @include('layouts.partials.newsletter')
 
 @endsection
--- resources/views/layouts/partials/head.blade.php
+++ resources/views/layouts/partials/head.blade.php
@@ -2,7 +2,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 <meta name="description" content="">
 <meta name="author" content="">
-<title></title>
+<title>ChainGang</title>
 <!-- Bootstrap core CSS -->
 <link rel="stylesheet" href="css/app.css">
 <link rel="stylesheet" href="css/header.css">
@@ -13,5 +13,9 @@
 <link rel="stylesheet" href="css/admin.css">
 <link rel="stylesheet" href="css/products.css">
 <link rel="stylesheet" href="css/article.css">
+
+<!-- Font awesome -->
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
+
 <!-- Custom styles for this template -->
 <link href="https://fonts.googleapis.com/css?family=Montserrat:400,400i,500,500i,600,600i,700,700i,800,800i" rel="stylesheet">
