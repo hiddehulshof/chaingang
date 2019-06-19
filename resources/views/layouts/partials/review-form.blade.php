682a374c343f316df4780aec97ef3fce076cc094
Niels Bosman <nielsbosman33@icloud.com>
2019-06-19 10:41:14 +0200

    Changes

--- resources/views/layouts/partials/review-form.blade.php
+++ resources/views/layouts/partials/review-form.blade.php
@@ -5,27 +5,6 @@
             <form method="post" class="w-100">
                 <div class="row">
                     <div class="col-md-6 form-group">
-                        <label class="contact__label" for="InputFirstname">Voornaam *</label>
-                        <input type="text" class="form-control" name="firstname" id="InputFirstname"
-                               aria-describedby="firstname" placeholder="Voornaam" required>
-                    </div>
-                    <div class="col-md-6 form-group">
-                        <label class="contact__label" for="InputLastname">Achternaam *</label>
-                        <input type="text" class="form-control" name="lastname" id="InputLastname"
-                               aria-describedby="lastname"
-                               placeholder="Achternaam" required>
-                    </div>
-                    <div class="col-md-6 form-group">
-                        <label class="contact__label" for="InputTelephone">Telefoonnummer</label>
-                        <input type="tel" class="form-control" name="phonenumber" id="InputTelephone"
-                               aria-describedby="telephone" placeholder="Telefoonnummer">
-                    </div>
-                    <div class="col-md-6 form-group">
-                        <label  class="contact__label" for="InputEmail">E-mailadres *</label>
-                        <input type="email" class="form-control" name="e-mail" id="InputEmail" aria-describedby="email"
-                               placeholder="Emailadres" required>
-                    </div>
-                    <div class="col-md-6 form-group">
                         <label class="contact__label" for="InputSubject">Titel *</label>
                         <input type="text" class="form-control" name="subject" id="InputSubject" aria-describedby="subject"
                                placeholder="Titel" required>
